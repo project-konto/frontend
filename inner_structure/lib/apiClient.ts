96b4edac6dad9aad7251ec95888f06d602897a54
Quatov <nooasarano21@gmail.com>
2025-11-26 17:24:47 +0500

    A frontend project is initialized

---
+++ inner_structure/lib/apiClient.ts
@@ -0,0 +1,20 @@
+﻿import axious from "axios";
+import axios from "axios";
+
+export const apiClient = axios.create({
+    baseURL: process.env.NEXT_PUBLIC_API_URL,
+    withCredentials: true,
+})
+
+apiClient.interceptors.request.use((config) => {
+    return config;
+})
+
+apiClient.interceptors.response.use(
+    (response) => response,
+    (error) => {
+        if (error.response?.status === 401) {}
+        
+        return Promise.reject(error);
+    }
+)
---
+++ inner_structure/types/dto.ts
@@ -0,0 +1,24 @@
+﻿export interface UserDto {
+    id: string
+    name: string
+    email: string
+}
+
+export type TransactionType = "Income" | "Expense" | "Transfer";
+
+export interface TransactionDto {
+    id: string
+    type: TransactionType
+    amount: number
+    currency: string
+    category: string
+    date: string
+    description: string
+}
+
+export interface BudgetDto {
+    id: string
+    currentBalance: number
+    currency: string
+    transactionsCount: number
+}
